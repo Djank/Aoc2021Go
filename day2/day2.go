47601fdd1dc3782491f9a88f54ac9d59df0db8cf
Djank <ankudinoff@gmail.com>
2021-12-03 12:27:30 +0300

    Solution and tests for Day2

--- day2/day2.go
+++ day2/day2.go
@@ -1,26 +1,85 @@
 package aoc
 
 import (
+	"log"
+	"strconv"
+	"strings"
+
 	"github.com/djank/aoc2021go/utils"
 )
 
 func Part1() int {
 	input := utils.ReadFileToString("day2/input.txt")
-
-	return part1Solution(input)
+	lines := utils.InputToStrings(input)
+	return part1Solution(lines)
 }
 
 func Part2() int {
 	input := utils.ReadFileToString("day2/input.txt")
-
-	return part2Solution(input)
+	lines := utils.InputToStrings(input)
+	return part2Solution(lines)
 }
 
-func part1Solution(inp string) int {
-	panic("part 1 not implemented")
+func part1Solution(lines []string) int {
+	x, z := 0, 0
+	for _, line := range lines {
+		dx, dz := parseLine(line)
+		x += dx
+		z += dz
+	}
+	return x * z
 }
 
-func part2Solution(inp string) int {
-	panic("part 2 not implemented")
+func parseLine(line string) (int, int) {
+	line = strings.Trim(line, "\r\n")
+	parts := strings.Split(line, " ")
+	if len(parts) != 2 {
+		log.Fatalf("Error on parsing line %s", line)
+	}
+
+	value, err := strconv.Atoi(parts[1])
+	if err != nil {
+		log.Printf("Error on atoi of line %s", line)
+		log.Fatal(err)
+	}
+
+	switch parts[0] {
+	case "forward":
+		return value, 0
+	case "down":
+		return 0, value
+	case "up":
+		return 0, -value
+	default:
+		log.Fatalf("%s not recogized. Line %s", parts[0], line)
+	}
+	return 0, 0
 }
 
+func part2Solution(lines []string) int {
+	hor, aim, depth := 0, 0, 0
+	for _, line := range lines {
+		line = strings.Trim(line, "\r\n")
+		parts := strings.Split(line, " ")
+		if len(parts) != 2 {
+			log.Fatalf("Error on parsing line %s", line)
+		}
+		value, err := strconv.Atoi(parts[1])
+		if err != nil {
+			log.Printf("Error on atoi of line %s", line)
+			log.Fatal(err)
+		}
+		switch parts[0] {
+		case "forward":
+			hor += value
+			depth += aim * value
+		case "down":
+			aim += value
+		case "up":
+			aim -= value
+		default:
+			log.Fatalf("%s not recogized. Line %s", parts[0], line)
+		}
+	}
+	return hor * depth
+}
--- day2/day2_test.go
+++ day2/day2_test.go
@@ -1,37 +1,59 @@
 package aoc
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/djank/aoc2021go/utils"
+)
 
 func Test_part1Solution(t *testing.T) {
-	type args struct {
-		inp string
-	}
 	tests := []struct {
 		name string
-		args args
+		args string
+		want int
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Example",
+			args: `forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2`,
+			want: 150,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			part1Solution(tt.args.inp)
+			if got := part1Solution(utils.InputToStrings(tt.args)); got != tt.want {
+				t.Errorf("part1Solution() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
 
 func Test_part2Solution(t *testing.T) {
-	type args struct {
-		inp string
-	}
 	tests := []struct {
 		name string
-		args args
+		args string
+		want int
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Example",
+			args: `forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2`,
+			want: 900,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			part2Solution(tt.args.inp)
+			if got := part2Solution(utils.InputToStrings(tt.args)); got != tt.want {
+				t.Errorf("part1Solution() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
