799eecc8679642a4f4b543d73a17cf4d0f0f77f5
Evgeny Ankudinov <ankudinoff@gmail.com>
2021-12-20 18:16:50 +0300

    Day 5 Part 1 solution with test

--- day5/day5.go
+++ day5/day5.go
@@ -1,25 +1,97 @@
 package aoc
 
 import (
+	"log"
+	"strings"
+
 	"github.com/djank/aoc2021go/utils"
 )
 
 func Part1() int {
-	input := utils.ReadFileToString("day4/input.txt")
+	input := utils.ReadFileToString("day5/input.txt")
 	lines := utils.InputToStrings(input)
-	return part1Solution(lines)
+	return part1Solution(lines, 1000)
 }
 
 func Part2() int {
-	input := utils.ReadFileToString("day4/input.txt")
+	input := utils.ReadFileToString("day5/input.txt")
 	lines := utils.InputToStrings(input)
-	return part2Solution(lines)
+	return part2Solution(lines, 1000)
 }
 
-func part1Solution(lines []string) int {
+func part1Solution(lines []string, dimension int) int {
+	lns := parseInput(lines)
+	arr := createArray2D(dimension)
+	for _, l := range lns {
+		if l.from.x != l.to.x && l.from.y != l.to.y {
+			continue
+		}
+		x1, x2 := sortInt(l.from.x, l.to.x)
+		y1, y2 := sortInt(l.from.y, l.to.y)
+		for x := x1; x <= x2; x++ {
+			for y := y1; y <= y2; y++ {
+				arr[x][y] += 1
+			}
+		}
+	}
+	return countGreateThen2(arr)
+}
+
+func part2Solution(lines []string, dimension int) int {
 	return 0
 }
 
-func part2Solution(lines []string) int {
-	return 0
+func countGreateThen2(arr [][]int) int {
+	count := 0
+	for _, xx := range arr {
+		for _, x := range xx {
+			if x > 1 {
+				count += 1
+			}
+		}
+	}
+	return count
+}
+
+func sortInt(a int, b int) (int, int) {
+	if a > b {
+		return b, a
+	}
+	return a, b
+}
+
+func createArray2D(dimension int) [][]int {
+	arr := make([][]int, dimension)
+	for i, _ := range arr {
+		arr[i] = make([]int, dimension)
+	}
+	return arr
+}
+
+func parseInput(lines []string) []Line {
+	var result []Line
+	for i, v := range lines {
+		ll := strings.Split(strings.Trim(v, "\r"), " -> ")
+		if len(ll) != 2 {
+			log.Panicln("Parse trouble. Line ", i)
+		}
+		from := utils.SplitLineToIntegers(ll[0], ",")
+		to := utils.SplitLineToIntegers(ll[1], ",")
+		line := Line{
+			from: Point{x: from[0], y: from[1]},
+			to:   Point{x: to[0], y: to[1]},
+		}
+		result = append(result, line)
+	}
+	return result
+}
+
+type Point struct {
+	x int
+	y int
+}
+
+type Line struct {
+	from Point
+	to   Point
 }
--- day5/day5_test.go
+++ day5/day5_test.go
@@ -2,13 +2,16 @@ package aoc
 
 import (
 	"testing"
+
 	"github.com/djank/aoc2021go/utils"
 )
+
 func Test_part1Solution(t *testing.T) {
 	tests := []struct {
-		name string
-		args string
-		want int
+		name      string
+		args      string
+		dimension int
+		want      int
 	}{
 		{
 			name: "Example",
@@ -22,13 +25,46 @@ func Test_part1Solution(t *testing.T) {
 3,4 -> 1,4
 0,0 -> 8,8
 5,5 -> 8,2`,
-			want: 5,
+			dimension: 10,
+			want:      5,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := part1Solution(utils.InputToStrings(tt.args)); got != tt.want {
+			if got := part1Solution(utils.InputToStrings(tt.args), tt.dimension); got != tt.want {
 				t.Errorf("part1Solution() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_part2Solution(t *testing.T) {
+	tests := []struct {
+		name      string
+		args      string
+		dimension int
+		want      int
+	}{
+		{
+			name: "Example",
+			args: `0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2`,
+			dimension: 10,
+			want:      12,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part2Solution(utils.InputToStrings(tt.args), tt.dimension); got != tt.want {
+				t.Errorf("part2Solution() = %v, want %v", got, tt.want)
 			}
 		})
 	}
