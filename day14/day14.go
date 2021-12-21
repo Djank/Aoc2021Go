5002efdb182b9c06f7833b46ea2efcbbc0bfcaf6
Evgeny Ankudinov <ankudinoff@gmail.com>
2021-12-21 20:15:45 +0300

    Day 14 part 1 progress

--- day14/day14.go
+++ day14/day14.go
@@ -1,6 +1,9 @@
 package aoc
 
 import (
+	"log"
+	"strings"
+
 	"github.com/djank/aoc2021go/utils"
 )
 
@@ -17,7 +20,38 @@ func Part2() int {
 }
 
 func part1Solution(lines []string) int {
-	panic("unimplemented")
+	list, transform := ParseInput(lines)
+	log.Println("input list of ", len(list))
+	log.Println("Transforms of ", len(transform))
+	panic("notimplemented")
+}
+
+func ParseInput(lines []string) ([]Sym, map[string]rune) {
+	firstLine := strings.Trim(lines[0], "\r")
+	var syms []Sym
+	var old Sym
+	for _,ch := range firstLine {
+		cur := Sym { char: ch, next: -1 }
+		old.next = len(syms)
+		syms = append(syms, cur)
+		old = cur
+	}
+
+	var trans map[string]rune
+	for _, line := range lines[3:] {
+		parts := strings.Split(line," -> ")
+		trans[parts[0]] = rune(parts[1][0])
+	}
+	return syms, trans
+}
+
+type Sym struct {
+	char rune
+	next int
+}
+
+type SymList struct {
+	syms []Sym
 }
 
 func part2Solution(lines []string) int {
